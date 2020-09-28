ae3dd096d1936e1fd29331a184bc1819f54218ad
Karnati Santosh <karnati.santosh@dxc.com>
2020-09-28 12:40:14 +0530

    NewMethod

--- MyLibraryStore/Controllers/CustomersController.cs
+++ MyLibraryStore/Controllers/CustomersController.cs
@@ -12,5 +12,10 @@ namespace MyLibraryStore.Controllers
         {
             return View();
         }
+
+        public IActionResult Action()
+        {
+            return Content("Hello Sir........");
+        }
     }
 }
